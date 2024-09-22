2dc5e44d936f1b6095227396da360bd30cd85300
tester <polandtiara62@gmail.com>
2024-09-22 10:27:06 +0200

    commit_lesson_05

--- src/main/kotlin/com/feefield/kotlincourse/classworks/class_lesson05.kt
+++ src/main/kotlin/com/feefield/kotlincourse/classworks/class_lesson05.kt
@@ -1,8 +1,37 @@
 package com.feefield.kotlincourse.classworks
 
-fun main() {
-    val sum = 10 + 5
+//fun main() {
+//    val sum = "10" + "5"
+//    val isEqual = 5 == 4
+//    val isNotEqual = 6 != 5
+//
+//    val andResult = true && false
+//    val orResult = true || false
+//    val notResult = !(5 > 3)
+//
+//    var number  = 5
+//    number -= 2
+//
+//    println(number)
 
-    println(sum)
+//        printVolume(25)
+//
+//    }
+//
+//    fun printVolume(userVolume: Int?) {
+//        val defaultVolume = 30
+//        println(userVolume ?: defaultVolume)
+//    }
+
+fun main() {
+    printPrice(150.5, null)
+    printPrice(150.5, 7)
 
 }
+
+    fun printPrice(price: Double, d: Int?){
+        val koef = (100 -(d?:0))/100.0
+        println(price * koef)
+    }
+
+
