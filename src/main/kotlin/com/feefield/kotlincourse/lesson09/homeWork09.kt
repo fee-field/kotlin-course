fa70ba39dc85644d45648344c656d1f952014462
tester <polandtiara62@gmail.com>
2024-10-02 22:05:25 +0200

    commit_lesson_08_01

--- src/main/kotlin/com/feefield/kotlincourse/lesson09/homeWork09.kt
+++ src/main/kotlin/com/feefield/kotlincourse/lesson09/homeWork09.kt
@@ -3,18 +3,19 @@ package com.feefield.kotlincourse.lesson09
 fun main() {
 
     var i = 0
-    val songPhrase = "Happy Birthday to you"
+    val songPhrase = "Happy Birthday to you,"
     val name = "Sasha"
 
     while (i < 3) {
         if (i == 2) {
             println("Happy Birthday, dear $name,")
         } else {
-            println("$songPhrase,")
+            println("$songPhrase")
         }
         i++
     }
     if (songPhrase.startsWith("Happy Birthday")) {
-        println("$songPhrase!")
+
+        println(songPhrase.replace(",", "!"))
     }
 }
