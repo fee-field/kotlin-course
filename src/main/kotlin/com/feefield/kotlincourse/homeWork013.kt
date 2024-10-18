2002dff11996a75a71c7a6854c0283d4606ebc00
tester <polandtiara62@gmail.com>
2024-10-18 16:38:59 +0200

    commit_lesson_13 // the second part

--- src/main/kotlin/com/feefield/kotlincourse/homeWork013.kt
+++ src/main/kotlin/com/feefield/kotlincourse/homeWork013.kt
@@ -1,10 +1,10 @@
 package com.feefield.kotlincourse
 
 fun main() {
-
+    val collect01 = listOf(1, 3, 5, 6, 7, 9, 19, 27, 19)
 
 //Проверить, что размер коллекции больше 5 элементов
-    val collect01 = listOf(1, 3, 5, 6, 7, 9)
+
     if (collect01.size > 5) {
         println("true")
     } else {
@@ -58,4 +58,60 @@ fun main() {
         } else {
             println("does not contain $i")
     }
+
+// Задачи на обработку коллекций
+
+    //Отфильтровать коллекцию по диапазону 18-30
+    val range1 = collect01.filter { it in 18..30 }
+    println(range1)
+
+    //Выбрать числа, которые не делятся на 2 и 3 одновременно
+    val divide1 = collect01.filterNot {it % 2 == 0 || it % 3 == 0}
+    println(divide1)
+
+    //Очистить текстовую коллекцию от null элементов
+    val strings = listOf("one", "two", "three", "four", "five", null, "six", null)
+    val strings1 = listOf("one", "two", "three", "four", "five")
+    val removeNulls = strings.filterNotNull()
+    println(removeNulls)
+
+    //Преобразовать текстовую коллекцию в коллекцию длин слов
+    val length = strings1.map{it.length}
+    println("$length - длины слов")
+
+    //Преобразовать текстовую коллекцию в мапу, где ключи - слова, а значения - перевёрнутые слова
+    val maps = strings1.associate { it to it.reversed() }
+    println("$maps - перевернутые слова")
+
+    //Отсортировать список в алфавитном порядке
+
+    val sorted = strings1.sorted()
+    println(sorted)
+
+    //Отсортировать список по убыванию
+    val sortedDesc = collect01.sortedDescending()
+    println(sortedDesc)
+
+    //Распечатать квадраты элементов списка
+    val double = collect01.forEach({println(it * it)})
+
+    //Группировать список по первой букве слов
+    val group = strings.groupBy {  }
+
+    //Очистить список от дублей
+    val distinct = collect01.distinct()
+    println(distinct)
+
+    //Взять первые 3 элемента списка
+    val last = collect01.take(3)
+    println(last)
+
+    //Взять последние 3 элемента списка
+    val last1 = collect01.takeLast(3)
+    println(last1)
+
+
+
+
+
 }
